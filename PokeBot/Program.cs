1368238908a7476d6b4e58cbbb483d688d291df7
davidean <pokecomfirmed@gmail.com>
2018-06-29 23:08:48 -0700

    the bot is now online
    trough the power of code the bot can now be online!!!!!! yay

--- PokeBot/Program.cs
+++ PokeBot/Program.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.IO;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
 
 namespace PokeBot
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private DiscordSocketClient client;
+        private CommandService command;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+
+
+        private async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug,
+            });
+
+            command = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug,
+
+            });
+
+
+            client.MessageReceived += client_messageRecieved;
+            await command.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            client.Ready += client_ready;
+            client.Log += client_log;
+
+
+            string token = "";
+            using (var stream = new FileStream(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.0", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(stream))
+            {
+                token = ReadToken.ReadToEnd();
+            }
+
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task client_log(LogMessage message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {message.Source} {message.Message}");
+
+        }
+
+        private async Task client_ready()
+        {
+            await client.SetGameAsync("p.help for help");
+        }
+
+        private async Task client_messageRecieved(SocketMessage arg)
+        {
+            //configure commands here.
         }
     }
-}
+
+
+    }
+
+
+
+    
+
+
+
